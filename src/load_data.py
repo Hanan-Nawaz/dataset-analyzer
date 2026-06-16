d3a9c07fde82851d6a6700c6957e85eb3b8616ef
Abdul Hanan Nawaz <hanannawaz0@gmail.com>
2026-06-17 00:08:01 +0200

    feat: update load_data

--- src/load_data.py
+++ src/load_data.py
@@ -1,1 +1,26 @@
 import pandas as pd
+
+def load_data(file_path: str) -> pd.DataFrame:
+    """Convert file into the Dataframe
+
+    Parameters
+    ----------
+    file_path : str
+        path of the dataset file
+
+    Returns
+    -------
+    pd.DataFrame
+        Dataframe of the dataset 
+    """        
+
+    try:
+
+        if "csv" in file_path or ".xlsx" in file_path:
+            df = pd.read_csv(file_path, index_col=0)
+        else:
+            print("The file must be csv or excel file")
+    except Exception as e:
+        print(f"The error is {e}")
+
+    return df
